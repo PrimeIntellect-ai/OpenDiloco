ef7af427cf9613e9d01e79c4f57ca233da0031b3
Sami Jaghouar <sami.jaghouar@hotmail.fr>
2024-07-19 15:54:33 +0000

    add diloco local/global step handling for NO_SHARD strategy

--- open_diloco/fsdp_diloco_native.py
+++ open_diloco/fsdp_diloco_native.py
@@ -6,18 +6,16 @@ import torch
 import wandb
 from pydantic_config import parse_argv, BaseConfig
 from torch.distributed import destroy_process_group, init_process_group
+import torch.distributed as dist
 
-from torch.distributed.fsdp import (
-    FullyShardedDataParallel as FSDP,
-    MixedPrecision,
-)
+from torch.distributed.fsdp import FullyShardedDataParallel as FSDP, MixedPrecision, ShardingStrategy
 from hivemind.optim.optimizer import logger
 
 
 from open_diloco.utils import get_sharding_strategy
 
-from torch.distributed.algorithms._comm_hooks.default_hooks import fp16_compress_hook, LowPrecisionState
-from torch.distributed.distributed_c10d import _get_default_group
+from torch.distributed.device_mesh import DeviceMesh
+from torch.distributed.algorithms._comm_hooks.default_hooks import DefaultState, reduce_scatter_hook, allreduce_hook
 
 
 # Function to initialize the distributed process group
@@ -30,6 +28,10 @@ def log(message):
     logger.info(f"[rank {os.environ['LOCAL_RANK']}] {message}")
 
 
+class DilocoConfig(BaseConfig):
+    num_inner_steps: int = 5
+
+
 class Config(BaseConfig):
     lr: float = 4e-4
     total_batch_size: int = 512
@@ -37,17 +39,67 @@ class Config(BaseConfig):
     sharding_strategy: str = "NO_SHARD"
     precision: Literal["fp16-mixed", "bf16-mixed", "32-true"] = "fp16-mixed"
     max_steps: int | None = None
+    n_layers: int = 4
+    diloco: DilocoConfig = DilocoConfig()
 
 
-def fp16_compress_hook_custom(state: LowPrecisionState, grad: torch.Tensor, output: Optional[torch.Tensor] = None):  # noqa: F821
-    log("here")
-    return fp16_compress_hook(state, grad, output)
+class DilocoState:
+    """
+    DilocoState is used to determine whether sync the (pseudo-)gradient globally or the gradient locally.
+
+    Args:
+        num_inner_steps: int - the number of inner steps
+        device_mesh: DeviceMesh - the device mesh to describe the communication topology. Should have only two dimension.
+            inter-node is the outermost dimension, and the intra-node is the innermost dimension.
+
+    """
+
+    __slots__ = ["current_step", "num_inner_steps", "intra_node_comm_state", "inter_node_comm_state"]
+
+    def __init__(self, num_inner_steps: int, device_mesh: DeviceMesh):
+        self.current_step = 0
+        self.num_inner_steps = num_inner_steps
+
+        assert isinstance(device_mesh, DeviceMesh) and device_mesh.ndim == 2
+
+        self.intra_node_comm_state = DefaultState(process_group=device_mesh.get_group(mesh_dim=1))
+        print(f"intra_node_comm_state: {dist.get_world_size(self.intra_node_comm_state.process_group)}")
+        self.inter_node_comm_state = DefaultState(process_group=device_mesh.get_group(mesh_dim=0))
+        print(f"inter_node_comm_state: {dist.get_world_size(self.inter_node_comm_state.process_group)}")
+
+    @property
+    def should_sync_globally(self):
+        return self.current_step % self.num_inner_steps == 0
+
+    def increment_current_step(self):
+        if self.current_step == self.num_inner_steps:
+            self.current_step = 0
+        else:
+            self.current_step += 1
+
+
+def diloco_comm_hook(state: DilocoState, grad: torch.Tensor, output: Optional[torch.Tensor] = None):
+    comm_state = state.intra_node_comm_state if state.should_sync_globally else state.inter_node_comm_state
+
+    if state.should_sync_globally:
+        log(f"Syncing global state {state.current_step}")
+    else:
+        log(f"Syncing local state {state.current_step} / {state.num_inner_steps}")
+
+    if output is not None:
+        reduce_scatter_hook(comm_state, grad, output)
+    else:
+        allreduce_hook(comm_state, grad)
+
+    state.increment_current_step()
 
 
 def train(config: Config):
     sharding_strategy = get_sharding_strategy(config.sharding_strategy)
-    local_rank = int(os.environ["LOCAL_RANK"])
+    _local_rank = int(os.environ["LOCAL_RANK"])
     world_size = int(os.environ["WORLD_SIZE"])
+    local_world_size = int(os.environ["LOCAL_WORLD_SIZE"])
+
     _rank = int(os.environ["RANK"])
 
     # batch_size is the total batch size for all GPUs
@@ -60,21 +112,37 @@ def train(config: Config):
     INPUT_DIM = 100
     OUTPUT_DIM = 100
 
-    model = torch.nn.Linear(INPUT_DIM, OUTPUT_DIM)
-    model = model.to(local_rank)
+    model = torch.nn.Sequential(*(torch.nn.Linear(INPUT_DIM, OUTPUT_DIM, bias=True) for _ in range(config.n_layers)))
+    model = model.to("cuda")
 
     half_precision = config.precision == "fp16-mixed" or config.precision == "bf16-mixed"
     half_precision_dtype = torch.bfloat16 if config.precision == "bf16-mixed" else torch.float16
     scaler = torch.cuda.amp.GradScaler(enabled=config.precision == "fp16-mixed")
+
+    nnodes = world_size // local_world_size
+    devices_mesh_diloco = DeviceMesh(
+        "cuda",
+        mesh=[[i * world_size + j for j in range(world_size)] for i in range(nnodes)],
+    )
+    print(f"devices_mesh_diloco: {devices_mesh_diloco}")
+
+    if sharding_strategy in [
+        ShardingStrategy._HYBRID_SHARD_ZERO2,
+        ShardingStrategy.HYBRID_SHARD,
+    ]:
+        device_mesh = devices_mesh_diloco
+    else:
+        device_mesh = None
 
     model = FSDP(
         model,
         sharding_strategy=sharding_strategy,
         mixed_precision=MixedPrecision(param_dtype=half_precision_dtype) if half_precision else None,
         use_orig_params=True,
+        device_mesh=device_mesh,
     )
-    state = LowPrecisionState(process_group=_get_default_group())
-    model.register_comm_hook(state=state, hook=fp16_compress_hook_custom)
+    state = DilocoState(num_inner_steps=config.diloco.num_inner_steps, device_mesh=devices_mesh_diloco)
+    model.register_comm_hook(state=state, hook=diloco_comm_hook)
 
     optimizer = torch.optim.AdamW(model.parameters(), lr=config.lr, weight_decay=0.1, betas=(0.9, 0.95))
 
