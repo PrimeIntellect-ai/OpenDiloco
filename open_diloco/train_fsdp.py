683b7e907664f0f983a985d5953f60b82c88b133
Sami Jaghouar <sami.jaghouar@hotmail.fr>
2024-08-02 14:38:58 +0000

    do not update lr scheduler during warmup

--- open_diloco/train_fsdp.py
+++ open_diloco/train_fsdp.py
@@ -198,15 +198,12 @@ def _get_cosine_schedule_with_warmup_lr_lambda(
     num_cycles: float,
     min_lr_rate: float = 0.0,
 ):
-    if (
-        warmup_outerstep is not None
-        and current_step > num_warmup_steps
-        and current_step % num_inner_steps < warmup_outerstep
-    ):
-        return 0
-
     if current_step < num_warmup_steps:
         return float(current_step) / float(max(1, num_warmup_steps))
+
+    if warmup_outerstep is not None and current_step % num_inner_steps < warmup_outerstep:
+        return 0
+
     progress = float(current_step - num_warmup_steps) / float(max(1, num_training_steps - num_warmup_steps))
     factor = 0.5 * (1.0 + math.cos(math.pi * float(num_cycles) * 2.0 * progress))
     factor = factor * (1 - min_lr_rate) + min_lr_rate
